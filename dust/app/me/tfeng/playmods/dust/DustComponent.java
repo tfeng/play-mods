6aa26a98a846547b3e21291331b49c61522778ee
Thomas Feng <tfeng@linkedin.com>
2015-07-15 22:33:29 -0700

    Refactor dust module; add support for using NodeJS as the JavaScript engine (in addition to Nashorn).

--- dust/app/me/tfeng/playmods/dust/DustComponent.java
+++ dust/app/me/tfeng/playmods/dust/DustComponent.java
@@ -20,63 +20,47 @@
 
 package me.tfeng.playmods.dust;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
-
-import javax.script.Bindings;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import javax.script.SimpleBindings;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.webjars.WebJarAssetLocator;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.io.CharStreams;
 
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.Futures;
-import me.tfeng.playmods.common.Constants;
+import me.tfeng.playmods.spring.Startable;
 import play.Logger;
-import play.Logger.ALogger;
 import play.libs.F.Promise;
 import scala.concurrent.ExecutionContextExecutorService;
-import scala.concurrent.Future;
 
 /**
  * @author Thomas Feng (huining.feng@gmail.com)
  */
 @Component("play-mods.dust.component")
-public class DustComponent implements InitializingBean {
+public class DustComponent implements InitializingBean, Startable {
 
-  public static final String DUST_JS_NAME = "dust-full.min.js";
+  private static final Logger.ALogger LOG = Logger.of(DustComponent.class);
 
-  public static final String RENDER_SCRIPT =
-      "dust.render(name, JSON.parse(json), function(err, data) {"
-          + "if (err) throw new Error(err); else writer.write(data, 0, data.length); })";
+  @Autowired
+  @Qualifier("play-mods.dust.asset-locator")
+  private AssetLocator assetLocator;
 
-  private static final ALogger LOG = Logger.of(DustComponent.class);
+  private EngineType engineType;
 
-  private WebJarAssetLocator assetLocator;
+  @Value("${play-mods.dust.js-engine-type:NASHORN}")
+  private String engineTypeName;
 
-  private ConcurrentLinkedQueue<ScriptEngine> engines;
+  private volatile ConcurrentLinkedQueue<JsEngine> engines;
 
-  private ExecutionContextExecutorService executionContext;
+  private volatile ExecutionContextExecutorService executionContext;
 
   @Value("${play-mods.dust.js-engine-pool-size:4}")
   private int jsEnginePoolSize;
@@ -84,97 +68,78 @@ public class DustComponent implements InitializingBean {
   @Value("${play-mods.dust.js-engine-pool-timeout:10000}")
   private long jsEnginePoolTimeout;
 
-  private final ObjectMapper mapper = new ObjectMapper();
+  @Value("${play-mods.dust.node-path:/opt/local/bin/node}")
+  private String nodePath;
 
   @Value("${play-mods.dust.templates-directory:templates}")
   private String templatesDirectory;
 
   @Override
   public void afterPropertiesSet() throws Exception {
-    assetLocator =
-        new WebJarAssetLocator(WebJarAssetLocator.getFullPathIndex(Pattern.compile(".*\\.js$"),
-            getClass().getClassLoader()));
+    engineType = EngineType.valueOf(engineTypeName);
+  }
 
-    engines = new ConcurrentLinkedQueue<>();
-    for (int i = 0; i < jsEnginePoolSize; i++) {
-      ScriptEngine engine = new ScriptEngineManager(null).getEngineByName("nashorn");
-      initializeScriptEngine(engine);
-      engines.offer(engine);
-    }
+  public AssetLocator getAssetLocator() {
+    return assetLocator;
+  }
 
+  public String getNodePath() {
+    return nodePath;
+  }
+
+  public String getTemplatesDirectory() {
+    return templatesDirectory;
+  }
+
+  @Override
+  public void onStart() throws Throwable {
     BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
-    ThreadPoolExecutor executor = new ThreadPoolExecutor(jsEnginePoolSize, jsEnginePoolSize,
-        jsEnginePoolTimeout, TimeUnit.MILLISECONDS, queue);
+    ThreadPoolExecutor executor = new ThreadPoolExecutor(jsEnginePoolSize, jsEnginePoolSize, jsEnginePoolTimeout,
+        TimeUnit.MILLISECONDS, queue);
     executor.setRejectedExecutionHandler((runnable, threadPoolExecutor) ->
         LOG.warn("JS engine rejected a request; executor " + threadPoolExecutor));
     executionContext = ExecutionContexts.fromExecutorService(executor);
+
+    initializeEngines();
+  }
+
+  @Override
+  public void onStop() throws Throwable {
+    engines.stream().forEach(JsEngine::destroy);
   }
 
   public Promise<String> render(String template, JsonNode data) {
-    Future<String> future = Futures.future(() -> {
-      ScriptEngine engine = engines.poll();
-
+    JsEngine engine = engines.poll();
+    return Promise.wrap(Futures.future(() -> {
       try {
-        boolean isRegistered = (Boolean) evaluate(engine, "dust.cache[template] !== undefined",
-            ImmutableMap.of("template", template));
-
-        if (!isRegistered) {
-          String jsFileName = templatesDirectory + "/" + template + ".js";
-          if (LOG.isDebugEnabled()) {
-            LOG.debug("Loading template " + jsFileName);
-          }
-
-          InputStream jsStream =
-              getClass().getClassLoader().getResourceAsStream(assetLocator.getFullPath(jsFileName));
-          String compiledTemplate = readAndClose(jsStream);
-          evaluate(engine, "dust.loadSource(source)", ImmutableMap.of("source", compiledTemplate));
-        }
-
-        if (LOG.isDebugEnabled()) {
-          LOG.debug("Rendering template " + template);
-        }
-
-        String json = mapper.writeValueAsString(data);
-        StringWriter writer = new StringWriter();
-        evaluate(engine, RENDER_SCRIPT,
-            ImmutableMap.of("name", template, "json", json, "writer", writer));
-        return writer.toString();
+        return engine.render(template, data);
       } finally {
-        engines.add(engine);
+        engines.offer(engine);
       }
-    }, executionContext);
-
-    return Promise.wrap(future);
+    }, executionContext));
   }
 
-  private Object evaluate(ScriptEngine engine, String script, Map<String, Object> data)
-      throws ScriptException {
-    Bindings bindings = new SimpleBindings(data);
-    engine.getContext().setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
-    return engine.eval(script);
+  public void switchToEngineType(EngineType engineType) throws Exception {
+    this.engineType = engineType;
+    initializeEngines();
   }
 
-  private void initializeScriptEngine(ScriptEngine engine) {
-    String dustJsPath = assetLocator.getFullPath(DUST_JS_NAME);
-    String dustJs = readAndClose(getClass().getClassLoader().getResourceAsStream(dustJsPath));
-    try {
-      engine.eval(dustJs);
-    } catch (ScriptException e) {
-      throw new RuntimeException("Unable to initialize script engine", e);
+  private JsEngine createEngine() throws Exception {
+    switch (engineType) {
+    case NASHORN:
+      return new NashornEngine(this);
+    case NODE:
+      return new NodeEngine(this);
+    default:
+      throw new RuntimeException("Unknown engine type " + engineType);
     }
   }
 
-  private String readAndClose(InputStream stream) {
-    try {
-      return CharStreams.toString(new InputStreamReader(stream, Constants.UTF8));
-    } catch (IOException e) {
-      throw new RuntimeException("Unable to read from stream", e);
-    } finally {
-      try {
-        stream.close();
-      } catch (IOException e) {
-        throw new RuntimeException("Unable to close stream", e);
-      }
+  private void initializeEngines() throws Exception {
+    ConcurrentLinkedQueue engines = new ConcurrentLinkedQueue<>();
+    for (int i = 0; i < jsEnginePoolSize; i++) {
+      engines.offer(createEngine());
     }
+    this.engines = engines;
   }
 }
