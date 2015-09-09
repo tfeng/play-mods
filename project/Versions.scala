09da92015d3df70e9b2780fc63583061f53d58ed
Thomas Feng <tfeng@linkedin.com>
2015-09-08 21:16:48 -0700

    Upgrade to toolbox 0.5.1-SNAPSHOT.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ package me.tfeng.playmods
 
 object Versions {
 
-  val project = "0.5.0"
+  val project = "0.5.1-SNAPSHOT"
 
   val avro = "1.7.7"
 
@@ -16,7 +16,7 @@ object Versions {
 
   val springSecurityOauth = "2.0.7.RELEASE"
 
-  val toolbox = "0.5.0"
+  val toolbox = "0.5.1-SNAPSHOT"
 
   val zookeeper = "3.4.6"
 }
