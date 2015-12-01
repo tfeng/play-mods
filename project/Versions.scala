81b7c2b229df360f7854cabe071f7e32d49e2c16
Thomas Feng <tfeng@linkedin.com>
2015-12-02 03:40:18 +0800

    Upgrade dependencies.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,21 +2,21 @@ package me.tfeng.playmods
 
 object Versions {
 
-  val project = "0.5.12"
+  val project = "0.5.14-SNAPSHOT"
 
   val avro = "1.7.7"
 
-  val httpComponents = "4.4.3"
+  val httpComponents = "4.4.4"
 
   val sbtPlugins = "0.5.9"
 
   val scala = "2.10.5"
 
-  val springSecurity = "4.0.2.RELEASE"
+  val springSecurity = "4.0.3.RELEASE"
 
-  val springSecurityOauth = "2.0.7.RELEASE"
+  val springSecurityOauth = "2.0.8.RELEASE"
 
-  val toolbox = "0.5.12"
+  val toolbox = "0.5.14-SNAPSHOT"
 
   val zookeeper = "3.4.6"
 }
