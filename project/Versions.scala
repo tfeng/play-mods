7b55dc41c596509e355dfc1d5028129e74a1fa2f
Thomas Feng <tfeng@linkedin.com>
2016-02-21 18:53:25 -0800

    Release 0.7.0.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,13 +2,13 @@ package me.tfeng.playmods
 
 object Versions {
 
-  val project = "0.7.0-SNAPSHOT"
+  val project = "0.7.0"
 
   val avro = "1.8.0"
 
   val httpComponents = "4.4.4"
 
-  val sbtPlugins = "0.7.0-SNAPSHOT"
+  val sbtPlugins = "0.7.0"
 
   val scala = "2.10.5"
 
@@ -16,7 +16,7 @@ object Versions {
 
   val springSecurityOauth = "2.0.9.RELEASE"
 
-  val toolbox = "0.7.0-SNAPSHOT"
+  val toolbox = "0.7.0"
 
   val zookeeper = "3.4.7"
 }
