6db29b0bb8fdda987f05ea7893ce16f705c122bf
Thomas Feng <tfeng@linkedin.com>
2016-05-09 23:58:40 -0700

    Upgrade dependencies.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ package me.tfeng.playmods
 
 object Versions {
 
-  val project = "0.8.3"
+  val project = "0.8.4-SNAPSHOT"
 
   val avro = "1.8.0"
 
@@ -10,11 +10,11 @@ object Versions {
 
   val scala = "2.11.7"
 
-  val springSecurity = "4.0.4.RELEASE"
+  val springSecurity = "4.1.0.RELEASE"
 
   val springSecurityOauth = "2.0.9.RELEASE"
 
-  val toolbox = "0.8.3"
+  val toolbox = "0.8.4"
 
   val zookeeper = "3.4.8"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,6 @@
 resolvers += "Typesafe repository" at "https://dl.bintray.com/typesafe/maven-releases/"
 
 // The Play plugin
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.1")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.3")
 
-addSbtPlugin("me.tfeng.sbt-plugins" % "avro" % "0.8.3")
+addSbtPlugin("me.tfeng.sbt-plugins" % "avro" % "0.8.4")
